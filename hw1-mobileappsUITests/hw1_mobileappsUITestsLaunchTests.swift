3a81d86cb5acad2d6ec4c7e7824f022566b639c6
Alexander Mendoza <alexandermen2017@fau.edu>
2023-09-16 12:30:13 -0400

    Initial Commit

---
+++ hw1-mobileappsUITests/hw1_mobileappsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  hw1_mobileappsUITestsLaunchTests.swift
+//  hw1-mobileappsUITests
+//
+//  Created by Alexander Mendoza on 9/16/23.
+//
+
+import XCTest
+
+final class hw1_mobileappsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
